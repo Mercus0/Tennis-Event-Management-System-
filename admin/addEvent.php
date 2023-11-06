01a7aa8e8edfc17c24b585304cc160750696018b
Mercus0 <yannaingedu@gmail.com>
2023-11-06 14:18:10 +0630

    Testing

--- admin/addEvent.php
+++ admin/addEvent.php
@@ -1,4 +1,5 @@
 <?php
+    // This is test
     include('../connection/connecter.php');
     $query="INSERT INTO events (name,description,s_date,e_date,location)values('$_POST[name]','$_POST[description]','$_POST[s_date]','$_POST[e_date]','$_POST[location]')";
     if(!mysqli_query($con,$query)){
